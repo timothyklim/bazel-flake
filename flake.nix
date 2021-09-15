dc554f036f560663c3a484c68f5cf5410be66ebb
Timothy <git@timothyklim.com>
2021-09-15 11:04:31 +0700

    chore: upgrade to 5.0.0-pre.20210907.1

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
     java.url = "github:TawasalMessenger/jdk-flake";
     src = {
-      url = "github:bazelbuild/bazel/5.0.0-pre.20210831.2";
+      url = "github:bazelbuild/bazel/5.0.0-pre.20210907.1";
       flake = false;
     };
   };
