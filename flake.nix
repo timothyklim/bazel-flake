a01d171461e77b4d9a195084bcd365738dc1feee
timothy <git@timothyklim.com>
2022-04-07 13:38:46 +0700

    chore: upgrade deps

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "Bazel flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/release-21.11";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
     java.url = "github:TawasalMessenger/jdk-flake";
@@ -12,15 +13,17 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, java, src }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils, java, src }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (
       system:
       let
         sources = with builtins; (fromJSON (readFile ./flake.lock)).nodes;
         pkgs = nixpkgs.legacyPackages.${system};
         jdk = java.packages.${system}.openjdk_18;
+        bazel_5 = nixpkgs-unstable.legacyPackages.${system}.bazel_5;
         bazel = import ./build.nix {
           inherit pkgs nixpkgs jdk src;
+          bazel_5 = bazel_5;
           version = sources.src.original.ref;
         };
         bazel-app = flake-utils.lib.mkApp { drv = bazel; };
@@ -35,7 +38,7 @@
         devShell = callPackage ./shell.nix {
           # inherit bazel src;
           inherit src;
-          bazel = pkgs.bazel_5;
+          bazel = bazel_5;
         };
         nixosModule = {
           nixpkgs.overlays = [ overlay ];
