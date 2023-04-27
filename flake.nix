ad288c6b4aa824ad3bb126e48a967a08a874e728
timothy <git@timothyklim.com>
2023-04-27 17:58:23 +0700

    chore: upgrade bazel to 7.0.0-pre.20230417.1

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
     java.url = "github:timothyklim/jdk-flake";
     src = {
-      url = "github:bazelbuild/bazel/7.0.0-pre.20230412.2";
+      url = "github:bazelbuild/bazel/7.0.0-pre.20230417.1";
       flake = false;
     };
   };
