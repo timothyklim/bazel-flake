2f4b031b7678bb2a2de7bbf1a8d057e63c9dea0c
Timothy Klim <git@timothyklim.com>
2023-08-28 15:46:26 +0000

    chore: switch back to JDK20

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
       let
         sources = with builtins; (fromJSON (readFile ./flake.lock)).nodes;
         pkgs = nixpkgs.legacyPackages.${system};
-        jdk = java.packages.${system}.openjdk_21;
+        jdk = java.packages.${system}.openjdk_20;
         bazel = import ./build.nix {
           inherit pkgs nixpkgs jdk src;
           version = sources.src.original.ref;
